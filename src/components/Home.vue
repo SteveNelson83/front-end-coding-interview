641e72423b15f5a670cbada693713d2284ede763
snelson <snelson@enquirieszoek.onmicrosoft.com>
2021-09-25 17:42:41 +0100

    table component created, with axios call to api

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,17 @@
 <template>
-  <div></div>
+  <div>
+  <Table />
+  </div>
 </template>
 
 <script>
+import Table from "./Table";
+
 export default {
   name: "Home",
+  components: {
+    Table,
+  },
 };
 </script>
 
---
+++ src/components/Table.vue
@@ -0,0 +1,30 @@
+<template>
+    <table></table>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      users: [],
+    };
+  },
+
+  methods: {
+    async getData() {
+      try {
+        const response = await this.$http.get(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        this.users = response.data;
+        console.log(this.users);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+  created() {
+    this.getData();
+  },
+};
+</script>
