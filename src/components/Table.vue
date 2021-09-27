714d993bac16dc8ec5f0d8012e7fb36201d17985
snelson <snelson@enquirieszoek.onmicrosoft.com>
2021-09-27 22:56:15 +0100

    made user details editable

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,61 +1,71 @@
 <template>
-    <table class="table">
-        <tr>
-            <th>Name</th>
-            <th>User Name</th>
-            <th>Email Address</th>
-            <th>Phone Number</th>
-            <th>Website</th>
-        </tr>
-        <tr v-for="(user) in users" :key="user.id">
-            <td>{{user.name}}</td>
-            <td>{{user.username}}</td>
-            <td>{{user.emailaddress}}</td>
-            <td>{{user.phone}}</td>
-            <td>{{user.website}}</td>
-        </tr>
-    </table>
+  <table class="table">
+    <tr>
+      <th>Name</th>
+      <th>User Name</th>
+      <th>Email Address</th>
+      <th>Phone Number</th>
+      <th>Website</th>
+    </tr>
+    <tr v-for="(user) in users" :key="user.id">
+      <td><input type="text" v-model="user.name" v-bind:disabled="user.disabled"/></td>
+      <td><input type="text" v-model="user.username" v-bind:disabled="user.disabled"/></td>
+      <td><input type="text" v-model="user.email" v-bind:disabled="user.disabled"/></td>
+      <td><input type="text" v-model="user.phone" v-bind:disabled="user.disabled"/></td>
+      <td><input type="text" v-model="user.website" v-bind:disabled="user.disabled"/></td>
+      <button type="button" @click="user.disabled = !user.disabled">EDIT</button>
+    </tr>
+  </table>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      users: [],
-    };
-  },
-
-  methods: {
-    async getData() {
-      try {
-        const response = await this.$http.get(
-          "https://jsonplaceholder.typicode.com/users"
-        );
-        this.users = response.data;
-        console.log(this.users);
-      } catch (error) {
-        console.log(error);
-      }
+  export default {
+    data() {
+      return {
+        users: [],
+        disabled: true
+      };
     },
-  },
-  created() {
-    this.getData();
-  },
-};
+
+    methods: {
+      async getData() {
+        try {
+          const response = await this.$http.get(
+            "https://jsonplaceholder.typicode.com/users"
+          );
+          this.users = response.data.map(v => ({...v, disabled: true}));
+          console.log(this.users);
+        } catch (error) {
+          console.log(error);
+        }
+      },
+    },
+    created() {
+      this.getData();
+    },
+  };
 </script>
 
 <style>
-table,
-th,
-td {
+  table,
+  th,
+  td {
     border: 1px solid;
-}
-
-table {
+  }
+  
+  table {
     background: blueviolet;
-}
-th,
-td {
+  }
+  th,
+  td {
     background: white;
-}
+  }
+  input:disabled {
+    background: black;
+    color: white;
+  }
+  input:enabled {
+    background: white;
+    color: black;
+  }
 </style>
